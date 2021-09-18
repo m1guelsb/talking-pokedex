d46770498ed2fef60ad7b4088b7e31563f459b5e
m1guelsb <miguelbarbosacs@gmail.com>
2021-09-18 17:30:31 -0300

    query and time search request

--- src/components/PokemonItem.jsx
+++
@@ -1,8 +0,0 @@
-export function PokemonItem(props) {
-  return (
-    <li>
-      <h2>{props.pokemon.name}</h2>
-      <a href={props.pokemon.url}>Sobre o pokemao</a>
-    </li>
-  );
-}
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,52 +1,57 @@
 import { useEffect, useState } from 'react';
-import { PokemonItem } from './PokemonItem';
+// import { PokemonItem } from './PokemonItem';
+
+import SearchInput from './SearchInput';
 
 export function PokemonList() {
-  const [pokemons, setPokemons] = useState([]);
+  const [pokemonInfo, setPokemonInfo] = useState( );
 
-  const [search, setSearch] = useState('')
-
-
-  const s = search && search.toLowerCase();
-  const filtered =
-    !pokemons || !s
-      ? pokemons
-      : pokemons.filter(({ name }) =>
-          name.toLowerCase().includes(s)
-        );
-
-
+  const [searchText, setSearchText] = useState('bulbasaur')
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon-species')
+
+    if (searchText) {
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${searchText}`)
       .then(response => response.json())
-      .then(pokemonData => {
-        setPokemons(pokemonData.results)
+      .then(response => {
+        setPokemonInfo(response)
       });
-  }, []);
-  console.log(pokemons)
+    }
+    
+  }, [searchText]);
+
 
   return (
     <>
-      <input
-          type="search"
-          value={search}
-          onChange={(ev) => setSearch(ev.target.value)}
-        />
-      <ul>
-        
+      <SearchInput
+      value={searchText} 
+      onChange={(searchtxt) => setSearchText(searchtxt)}
+      />
 
-      {filtered && (
-        <ul>
-          {filtered.map(pokemon => (
-              <PokemonItem key={pokemon.name} pokemon={pokemon} />
-          ))}
-        </ul>
-      )} 
-        {/* {pokemons.map(pokemon => {
-          return <PokemonItem key={pokemon.name} pokemon={pokemon} />
-        })} */}
-      </ul>
+      {/* {searchText && !pokemonInfo && <span>Procurando...</span> } */}
+      
+
+      {pokemonInfo !== undefined && (
+        <div>
+          <p><b>Nome: </b>{pokemonInfo.name}</p>
+          <p><b>Color: </b>{pokemonInfo.color.name}</p>
+
+          { pokemonInfo.egg_groups[1] ? ( //se o segundo tipo existe (se o pokemon tem 2 tipos) 
+            <p><b>Types: </b>{pokemonInfo.egg_groups[0].name} and  {pokemonInfo.egg_groups[1].name}</p>
+          ) : ( //se nao tiver só mostra o tipo 0
+            <p><b>Type: </b>{pokemonInfo.egg_groups[0].name}</p>
+          )}
+
+          <p><b>Where to find it: </b>{pokemonInfo.habitat.name}</p>
+          <p><b>Description: </b>{pokemonInfo.flavor_text_entries[1].flavor_text} 
+            <br/>
+            {pokemonInfo.flavor_text_entries[3].flavor_text} 
+            <br/>
+            {pokemonInfo.flavor_text_entries[5].flavor_text}
+          </p>
+        </div>
+      )}
+
     </>
   );
 }
